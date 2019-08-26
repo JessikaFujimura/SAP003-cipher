d88815ad9e8c144be2ae99b92e7df72330a96261
JessikaFujimura <jessikafujimura@gmail.com>
2019-08-26 13:00:08 -0300

    test do Eslint ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,39 @@
 window.cipher = {
-  Encode: Encode,
-  Decode: Decode
+  encode: encode,
+  decode: decode
 };
-function Encode(Msg, fator) {
+function encode (Msg, Fator) {
   let ConjASCII = [];
-  for (let i=0; i < Msg.length; i++){
-    ConjASCII.push (Msg.charCodeAt(i));
-    }
+  for ( let i=0; i < Msg.length; i++) {
+    ConjASCII.push(Msg.charCodeAt(i)+ Number(Fator));
+  }
   let ConjASCIIFator = [];
-  function SomaFator (item){
-    if(item <= 90 && item >= 65){
-      ConjASCIIFator.push(((item - 65 + fator) % 26 + 65))};
-    }
+  function SomaFator (item) {
+    ConjASCIIFator.push((item - 65 ) % 26 + 65);
+    
+  }
   ConjASCII.forEach(SomaFator);
   let ConjcodASCIIF = [];
-  function ConvASCII (item){
-    ConjcodASCIIF.push(String.fromCharCode(item.toString()));
+  function ConvASCII ( item) {
+    ConjcodASCIIF.push (String.fromCharCode(item.toString()));
   }
-  ConjASCIIFator.forEach(ConvASCII);
-  return ConjcodASCIIF.join('');
+  ConjASCIIFator.forEach (ConvASCII);
+  return ConjcodASCIIF.join ("");
 }
-function Decode(Codigo, Refator){
+function decode (Codigo, Refator) {
   let ConjCodigo = [];
-  for (let i=0; i < Codigo.length; i++){
-    ConjCodigo.push (Codigo.charCodeAt(i));
-    }
-  let ConjASCIIRFator = [];
-  function SubtFator (item){
-    if (item <= 90 && item >= 65){
-      ConjASCIIRFator.push(((item - 90 - Refator) % 26 + 90))};
-    }
+  for ( let i=0; i < Codigo.length; i++) {
+    ConjCodigo.push (Codigo.charCodeAt(i)- Number(Refator));
+  }
+  let ConjASCIIRFator = [ ];
+  function SubtFator (item) {
+    ConjASCIIRFator.push (((item - 90) % 26 + 90));
+  }
   ConjCodigo.forEach(SubtFator);
   let Conj = [];
-  function ConvL (item){
-    Conj.push(String.fromCharCode(item.toString()));
+  function ConvL ( item) {
+    Conj.push ( String.fromCharCode(item.toString()));
   }
   ConjASCIIRFator.forEach(ConvL);
-  return Conj.join('');
-  }
+  return Conj.join ("");
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 document.getElementById("Codificar").addEventListener("click", Cifrar);
-
-let Msg = document.getElementById("msg").value;
-let fator = parseInt(document.getElementById("fator").value);
-
+let Msg = document.getElementById("msg");
+const Fator = document.getElementById("fator");
 function Cifrar() {
-    document.getElementById("formulario").addEventListener("submit", function(event) {event.preventDefault()});
-    document.getElementById("msgcifrada").innerHTML = window.cipher.Encode(Msg, fator);
+  event.preventDefault();
+  document.getElementById("msgcifrada").innerHTML = window.cipher.encode(Msg.value, Fator.value);
 } 
 //document.getElementById("Limpar").addEventListener("click", ;
-
-
+document.getElementById("Decodificar").addEventListener("click", Decifrar);
 let Codigo = document.getElementById("codigo").value;
 let Refator = document.getElementById("refator").value;
-function Decifrar (){
-    event.preventDefault();
-    document.getElementById("msgdecifrada").innerHTML = window.cipher.Decode(Codigo, Refator);
+function Decifrar( ) {
+  event.preventDefault();
+  document.getElementById( "msgdecifrada").innerHTML = window.cipher.decode(Codigo, Refator);
 }
